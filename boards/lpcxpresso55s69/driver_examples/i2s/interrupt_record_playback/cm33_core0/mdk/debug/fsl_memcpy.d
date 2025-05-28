a4794333d76a66a434b80a7b1ba045f24a080b4b
unknown <duql@yusur.com>
2025-05-28 10:26:46 +0800

    init

---
+++ boards/lpcxpresso55s69/driver_examples/i2s/interrupt_record_playback/cm33_core0/mdk/debug/fsl_memcpy.d
@@ -0,0 +1,2 @@
+debug/fsl_memcpy.o: \
+  ..\..\..\..\..\..\..\devices\LPC55S69\utilities\fsl_memcpy.S
---
+++ boards/lpcxpresso55s69/driver_examples/i2s/interrupt_record_playback/cm33_core0/mdk/debug/startup_lpc55s69_cm33_core0.d
@@ -0,0 +1,2 @@
+debug/startup_lpc55s69_cm33_core0.o: \
+  ..\..\..\..\..\..\..\devices\LPC55S69\arm\startup_LPC55S69_cm33_core0.S
